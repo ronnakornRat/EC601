a9a0f515b0772704ed0c14063b3f15a810efc545
boss uengsamphanpong <ronrat@bu.edu>
2020-12-07 21:25:13 +0700

    update decrypt

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/my_secret.json my_secret.json.gpg
+--output $HOME/key/google_nlp_key.json google_nlp_key.json.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE_3" \
+--output $HOME/key/twitter_key.json twitter_key.json.gpg
