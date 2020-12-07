25d4161012b970a59184c38cbadfa54113982540
boss uengsamphanpong <ronrat@bu.edu>
2020-12-07 21:45:01 +0700

    change key location

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,7 +4,7 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/key/google_nlp_key.json google_nlp_key.json.gpg
+--output $HOME/key/google_nlp_key.json $HOME/key/google_nlp_key.json.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE_3" \
---output $HOME/key/twitter_key.json twitter_key.json.gpg
+--output $HOME/key/twitter_key.json $HOME/key/twitter_key.json.gpg
