b571ca07fbf60f9e4d823f96e5934cc2a8ffc40a
boss uengsamphanpong <ronrat@bu.edu>
2020-12-07 21:55:25 +0700

    edit workflow

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -6,5 +6,5 @@
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
 --output $HOME/key/google_nlp_key.json $HOME/key/google_nlp_key.json.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE_3" \
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE_2" \
 --output $HOME/key/twitter_key.json $HOME/key/twitter_key.json.gpg
