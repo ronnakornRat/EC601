5376c77a6593c571d86607c56d8d40b31c2a98ea
boss uengsamphanpong <ronrat@bu.edu>
2020-12-07 21:57:49 +0700

    change file location in script

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,7 +4,7 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/key/google_nlp_key.json $HOME/key/google_nlp_key.json.gpg
+--output ./key/google_nlp_key.json ./key/google_nlp_key.json.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE_2" \
---output $HOME/key/twitter_key.json $HOME/key/twitter_key.json.gpg
+--output ./key/twitter_key.json ./key/twitter_key.json.gpg
